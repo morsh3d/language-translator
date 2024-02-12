d55c5b66b864ffa3d431ca24feba3ee4d3ffe2a0
Yasmin Morshed <yasmin.morshed@madetech.com>
2024-02-12 16:36:14 +0000

    Play latest audio file each time. Improve UI.

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, request, render_template, url_for
-from french import load_model, translate_text
+import glob
+import os
+from flask import Flask, request, render_template
+from french import translate_text
 
 app = Flask(__name__)
-
-# Load the model and tokenizer once when the app starts
-model_name = "Helsinki-NLP/opus-mt-fr-en"
-model, tokenizer = load_model(model_name)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     translation = ''
-    audio_file = None
+    latest_audio_file = None
     if request.method == 'POST':
         text = request.form.get('text')
-        translation, audio_file = translate_text(model, tokenizer, text)
-    return render_template('index.html', translation=translation, audio_file=audio_file)
+        translation, audio_file = translate_text(text)
+
+        # Only get the latest audio file if there is a translation
+        if translation:
+            latest_audio_file = os.path.basename(audio_file)
+
+    return render_template('index.html', translation=translation, latest_audio_file=latest_audio_file)
 
 if __name__ == '__main__':
     app.run(debug=True)
